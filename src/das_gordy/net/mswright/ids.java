02c36c9320fda263526edaa7373f5bc171b10990
Gautom Das <gautomdas4@gmail.com>
2018-04-03 09:32:47 -0400

    General Updates

--- src/das_gordy/net/mswright/ids.java
+++ src/das_gordy/net/mswright/ids.java
@@ -1,8 +1,12 @@
 package das_gordy.net.mswright;
 
-public class ids
-{
+public class ids {
     public static void main(String[] args) {
-        
+
     }
+
+    public static readFile(String file) {
+
+    }
+
 }
